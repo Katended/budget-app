21507dc7b48851a44ef6c592b8d8ebc8c4a54371
David Katende <katendedavidjoga@gmail.com>
2023-09-28 15:43:12 +0300

    Add render file

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_19_132626) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "adminpack"
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
